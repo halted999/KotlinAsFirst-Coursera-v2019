c25ee938377400af160330200c4a064beeeb9d91
halted <halted999@gmail.com>
2020-04-08 13:01:24 +0300

    Решены задачи по лекции "Простые функции, выражения, типы"

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -45,23 +45,19 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
 fun daysInMonth(month: Int, year: Int): Int {
-    return when(month){
-        1 -> 31
-        2 -> (
-            if (year % 4 == 0) 29 else 28
-                )
-        3 -> 31
-        4 -> 30
-        5 -> 31
-        6 -> 30
-        7 -> 31
-        8 -> 31
-        9 -> 30
-        10 -> 31
-        11 -> 30
-        12 -> 31
-        else -> 0
+    return when(month) {
+        4, 6, 9, 11 -> 30
+        2 -> when ()
+        else -> 31
     }
+//    return when(year % 4 != 0){
+//        4 -> 30
+//        6 -> 30
+//        9 -> 30
+//        11 -> 30
+//        2 -> 28
+//        else -> 31
+//    }
     }
 
 /**
